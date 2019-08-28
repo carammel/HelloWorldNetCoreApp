cec2e3eb2d7bcbff215d4fe13c693e3f9215a991
carammel <121emelyildirim@gmail.com>
2019-08-28 15:59:32 +0300

    hola

---
+++ ConsoleAppTFS1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleAppTFS1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Can you tell me your name,pls?");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name);
+            Console.WriteLine("Please enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
